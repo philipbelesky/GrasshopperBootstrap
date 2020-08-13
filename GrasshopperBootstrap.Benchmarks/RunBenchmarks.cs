6de98f302d0a3c4da5d4bbf48b3ab0c20643c6cc
Philip Belesky <contact@philipbelesky.com>
2020-08-13 20:22:24 +1000

    Add Benchmarks project and items

---
+++ GrasshopperBootstrap.Benchmarks/RunBenchmarks.cs
@@ -0,0 +1,22 @@
+﻿namespace GrasshopperBootstrap.Benchmarks
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using SubCategory;
+
+    class RunBenchmarks
+    {
+        static void Main(string[] args)
+        {
+            RunGeometryCreationBenchmarks.Benchmarks();
+
+            // Prevent console from auto exiting
+            Console.Write("Note: Benchmark summaries saved to \\bin\\Release\\BenchmarkDotNet.Artifacts\\results.");
+            Console.Write("Finished Benchmarks. Press any key to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GrasshopperBootstrap.Benchmarks/SubCategory/GeometryCreationBenchmarks.cs
@@ -0,0 +1,43 @@
+﻿namespace GrasshopperBootstrap.Benchmarks.SubCategory
+{
+    using BenchmarkDotNet.Attributes;
+    using BenchmarkDotNet.Running;
+    using Rhino.Geometry;
+    using System.Collections.Generic;
+    using GrasshopperBootstrap.SubCategory;
+
+    public class RunGeometryCreationBenchmarks
+    {
+        public static void Benchmarks()
+        {
+            BenchmarkRunner.Run<GeometryCreationBenchmarks>();
+            GeometryCreationBenchmarks.TestCurrentImplementation();
+            GeometryCreationBenchmarks.TestProposedImplementation();
+        }
+    }
+
+    public class GeometryCreationBenchmarks
+    {
+        private static Plane stubPlane = new Plane(new Point3d(0, 0, 0), new Vector3d(0, 0, 1));
+
+        [Benchmark(Baseline = true)]
+        public static void TestCurrentImplementation()
+        {
+            var stubInnerRadius = 10.0;
+            var stubOuterRadius = 100.0;
+            var stubTurns = 5;
+            Curve spiral = GeometryCreation.CreateSpiral(stubPlane, stubInnerRadius, stubOuterRadius, stubTurns);
+        }
+
+        [Benchmark]
+        public static void TestProposedImplementation()
+        {;
+            var stubInnerRadius = 10.0;
+            var stubOuterRadius = 10000.0;
+            var stubTurns = 25;
+            Curve spiral = GeometryCreation.CreateSpiral(stubPlane, stubInnerRadius, stubOuterRadius, stubTurns);
+        }
+    }
+}
+
+
