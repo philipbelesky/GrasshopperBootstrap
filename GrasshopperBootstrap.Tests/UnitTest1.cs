4f2a43822067c3d3e4636b7788f41ef95acf9b0b
Philip Belesky <philip@belesky.com>
2021-04-13 15:16:00 +1000

    Split off RhinoTests (local) from Tests (CI)

---
+++ GrasshopperBootstrap.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace GrasshopperBootstrap.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // This method is just picked to show how to call into the GrasshopperBoostrap logics
+            var result = GrasshopperBootstrap.SubCategory.ModularityDemo.DummyMethodUsedByExampleTests(1, 11);
+            Assert.AreEqual(result, 12);
+        }
+    }
+}
